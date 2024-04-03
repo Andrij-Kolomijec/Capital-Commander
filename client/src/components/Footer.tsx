da2c89f306e918ffd9544ccb0bf12fe5f5e44745
Difficins <a.kolomijec@seznam.cz>
2024-04-03 21:58:52 +0200

    Added footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,22 @@
 import classes from "./Footer.module.css";
+import github from "../assets/github.svg";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={classes.footer}>
+      <a href="https://github.com/Andrij-Kolomijec">
+        <img src={github} alt="GitHub Icon" title="More of my work" />
+      </a>
+      <span>
+        Background image from&nbsp;
+        <a
+          title="Visit website"
+          className="link"
+          href="https://www.pexels.com/photo/empty-brown-canvas-235985/"
+        >
+          Pexels
+        </a>
+      </span>
+    </footer>
+  );
 }
