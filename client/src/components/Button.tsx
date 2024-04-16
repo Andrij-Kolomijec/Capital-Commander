81661a38072fc56b7b1e8bc3177e97d1f8a4786c
Difficins <a.kolomijec@seznam.cz>
2024-04-16 19:35:12 +0200

    Added loading indicator

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -14,35 +14,47 @@ export default function Button({
   loader = false,
   ...props
 }: ButtonProps) {
+  const transition = {
+    type: "spring",
+    duration: 0.3,
+    bounce: 0.4,
+    stiffness: 100,
+    mass: 1,
+    damping: 10,
+  };
+
+  const whileInteraction = !loader
+    ? {
+        scale: 1.1,
+        backgroundColor: "#00b5d4",
+      }
+    : {};
+
+  const whileTap = !loader ? { scale: 0.98 } : {};
+
   return (
-    <div className={loader ? classes.loader : ""}>
+    <motion.div
+      layout
+      transition={transition}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      // exit={{ opacity: 0, scale: 0 }}
+      className={loader ? classes.loader : ""}
+    >
       <motion.button
         className={classes.button}
-        layout
-        whileHover={{
-          scale: 1.1,
-          backgroundColor: "#00b5d4",
-        }}
-        whileFocus={{
-          scale: 1.1,
-          backgroundColor: "#00b5d4",
-        }}
-        whileTap={{ scale: 0.98 }}
-        transition={{
-          type: "spring",
-          duration: 0.3,
-          bounce: 0.4,
-          stiffness: 100,
-          mass: 1,
-          damping: 10,
-        }}
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0 }}
+        // layout
+        whileHover={whileInteraction}
+        whileFocus={whileInteraction}
+        whileTap={whileTap}
+        transition={transition}
+        // initial={{ opacity: 0, scale: 0 }}
+        // animate={{ opacity: 1, scale: 1 }}
+        // exit={{ opacity: 0, scale: 0 }}
         {...props}
       >
         {children}
       </motion.button>
-    </div>
+    </motion.div>
   );
 }
