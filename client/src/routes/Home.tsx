49b15dccde1f7f6b667e20720cad5496e0789fb0
Difficins <a.kolomijec@seznam.cz>
2024-04-16 20:39:58 +0200

    Added responsiveness

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,5 +1,9 @@
-// import classes from "./Home.module.css";
+import classes from "./Home.module.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={classes.home}>
+      <h1>Welcome!</h1>
+    </div>
+  );
 }
